2885ab146e24b168227c4afbfc314670cd1b26e7
Izabelle Cesario <izabellecesario@hotmail.com>
2023-09-21 16:35:45 -0300

    ap07(poo-p2): jogo v1

--- Personagem.java
+++ Personagem.java
@@ -1,18 +1,18 @@
 public class Personagem{
     // atributos
     String nome;
-    int energia;
-    int fome;
-    int sono;
+    int energia = 10;
+    int fome = 0;
+    int sono = 0;
 
     // comportamentos
     void cacar(){
         if(energia >= 2){
-            System.out.println(nome + " esta cacando...");
+            System.out.println(nome + " caçou");
             energia -= 2;
         }
         else{
-            System.out.println(nome + " esta sem energia para cacar...");
+            System.out.println(nome + " esta sem energia para cacar");
         }
         fome = Math.min(fome + 1,10);
         sono = Math.min(sono + 1,10);
@@ -20,22 +20,23 @@ public class Personagem{
 
     void comer(){
         if(fome >= 1){
-            System.out.println(nome + " esta comendo....");
+            System.out.println(nome + " comeu");
             fome -= 1;
             energia = Math.min(energia + 1,10);
         }
         else{
-            System.out.println(nome + " nao esta com fome para comer...");
+            System.out.println(nome + " nao esta com fome para comer");
         }  
     }
 
     void dormir(){
         if(sono > 0){
-            System.out.println(nome + " esta dormindo....");
+            System.out.println(nome + " dormiu");
             sono -= 1;
             energia = energia++ > 10 ? 10 : energia++;
         }
         else{
-            System.out.println(nome + " nao esta com sono para dormir...");
+            System.out.println(nome + " nao esta com sono para dormir");
         }
+    }
 }
